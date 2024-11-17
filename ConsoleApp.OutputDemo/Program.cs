a0dbf4dab44608a20267d40f59ec790efdbe72a8
swus1978 <juang4u@gmail.com>
2024-11-17 11:52:55 -0500

    Add project files.

---
+++ ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! My name is Juan Gustavo Quinones");
+Console.WriteLine("I am from Puerto Rico");
+Console.WriteLine("I love to code C#");
+Console.Write("Line 1");
+Console.Write("Line 2");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Difrent data trypes
+/*
+     text - string
+     integers - int
+     decimal - double, float, decimal
+     logical - bool
+
+ */
+
+string name = "Juan Quinones";
+
+Console.WriteLine(name);
+Console.WriteLine("I am" + name); // String concatenation
+Console.WriteLine($"They call me {name}"); // String interpolation
+Console.WriteLine("I was given the name {0}", name); // Formatted string
+
+int age  = 46;
+int retirementYearsLeft = 11;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetires  = false;
+Console.WriteLine("Am I retired? " + isRetires);
